697f210d64e9f31ba7aab07ec04855fe219645ea
lee93dhun <85606299+lee93dhun@users.noreply.github.com>
2024-05-08 20:47:12 +0900

    Title: 체질량지수, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240508/체질량지수/bmi.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt(), w = sc.nextInt();
+        int b = (10000*w)/(h*h);
+        if(b>=25) System.out.print(b+"\nObesity");
+    }
+}
