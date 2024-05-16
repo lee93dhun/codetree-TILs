ef01f05e027039a98584816643381884dd250cea
lee93dhun <85606299+lee93dhun@users.noreply.github.com>
2024-05-16 17:07:51 +0900

    Title: 높은 수에서 낮은 수까지, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240516/높은 수에서 낮은 수까지/from-high-to-low.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt();
+        int i,j =0;
+        if(a>b){
+            i=a;
+            j=b;
+        }else{
+            i=b;
+            j=a;
+        }
+        for(i=i;i>=j; i--){
+            System.out.print(i+" ");
+        }
+    }
+}
